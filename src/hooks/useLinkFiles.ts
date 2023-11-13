5d50dff605cf0bbc53cccede0bc39dcc2b3002d0
Esneider Ospina <sneuder8@gmail.com>
2023-11-13 02:03:52 +0000

    feat: update links for donwloads

--- src/hooks/useLinkFiles.ts
+++ src/hooks/useLinkFiles.ts
@@ -4,9 +4,9 @@ const useLinkFiles = () => {
   const donwloadFile = useMemo(() => {
     return {
       windows:
-        "https://drive.usercontent.google.com/uc?id=1xUJs65GxOIWOdwLdZ-Lp2IhoaLyaltEc&authuser=0&export=download",
+        "https://drive.usercontent.google.com/uc?id=1QEd0oDUwUVEWYR5jgWPobGynnfX7RM0t&authuser=0&export=download",
       linux:
-        "https://drive.usercontent.google.com/download?id=1IPuWdYxBWCg4O7mnE_1IQwUtrArKnML6&export=download&authuser=0&confirm=t&uuid=9372fe49-5d5b-4d06-9c96-3309de5a127a&at=APZUnTVqMzzbk8MV-EMTi8SJm4J1:1697485209143",
+        "https://drive.usercontent.google.com/uc?id=1JAjyW47ThnS7M9PR1r-6fvGtLBK7Gn2N&authuser=0&export=download",
     };
   }, []);
 
